803d0067f73147d6aa361f32343a9bc66c84a205
Jon Watts <jonwatts@activatecare.com>
2022-04-19 11:41:56 -0400

    made a changes to test2.py

--- app/test2.py
+++ app/test2.py
@@ -1,5 +1,7 @@
-def helloWord():
+def helloWorld():
 
     print(
         "Hello World"
     )  # a test comment that is too long in order to test the length flag test test test test
+
+print(helloWorld())
