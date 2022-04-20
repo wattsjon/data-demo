087643cd116a7bf9de6905723d7187762372e76d
Jon Watts <jonwatts@activatecare.com>
2022-04-20 18:40:59 +0000

    removed pre-commit hooks

--- app/containertest.py
+++ app/containertest.py
@@ -1,3 +1,19 @@
 #updated from the container
 
 #update 2
+
+def helloWorld():
+    """
+    Print hello world once.
+
+    :param: None
+    """
+
+
+
+
+    print("Hello World")  # this comment has been shortend
+
+
+
+helloWorld()
